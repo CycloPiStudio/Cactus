b6fa2455a5e62c46f0c229f6788989d63cce9626
CycloPi <venti3d@gmail.com>
2020-03-27 22:16:30 +0100

    puntos

--- Cactus/Ejecutable/Menu/Dios/Dios.gd
+++ Cactus/Ejecutable/Menu/Dios/Dios.gd
@@ -1,12 +1,17 @@
 extends Node
 var Nivel_jugar
+var puntosDios
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$"Music_menu".play()
-	print(Nivel_jugar)
+	print("Nivel juegar: " , Nivel_jugar," nivel")
+	print("Puntos dios: " , puntosDios ," puntos"  )
 
 	pass # Replace with function body.
+
+func sumaPuntos(puntosqsubes):
+	puntosDios += puntosqsubes
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
